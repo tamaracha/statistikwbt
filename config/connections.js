97cfd535544b9cd4c803438eee413f56ad23a16e
Tamara Cook <Tamara.Cook@icloud.com>
2014-11-30 02:19:03 +0100

    config repariert

--- config/connections.js
+++ config/connections.js
@@ -1,11 +1,10 @@
 module.exports.connections = {
-  mongodbServer: {
-    adapter: 'sails-mongo',
-    url: process.env.MONGOHQ_URL
-    //port: 27017,
-    // user: 'username',
-    // password: 'password',
-    //database: 'MyDatabase'
-  },
-
+	mongodbServer: {
+		adapter: 'sails-mongo',
+		host: "localhost",
+		//port: 27017,
+		// user: 'username',
+		// password: 'password',
+		database: 'MyDatabase'
+	}
 };
