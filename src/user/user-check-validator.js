0686965e3c0d97969ac5dfba10daf34ae7f13cc7
Tamara <Tamara.Cook@icloud.com>
2015-10-14 19:53:33 +0200

    * added parser to userCheck directive

--- src/user/user-check-validator.js
+++ src/user/user-check-validator.js
@@ -1,5 +1,8 @@
 export default /*@ngInject*/function userCheckValidator($q,user){
   function link(scope,el,attrs,c){
+    c.$parsers.push(function(value){
+    
+    });
     c.$asyncValidators.userCheck = function(value){
       const promise = user.check('email',value);
       switch(attrs.userCheck){
