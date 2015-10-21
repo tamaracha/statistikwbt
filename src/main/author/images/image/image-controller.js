bd9cdada6888d9cfa4bc82211b2e285a49a7188e
Tamara Cook <Tamara.Cook@icloud.com>
2015-10-22 01:27:36 +0200

    * author: fixed image template

--- src/main/author/images/image/image-controller.js
+++ src/main/author/images/image/image-controller.js
@@ -2,6 +2,6 @@ import _ from 'lodash';
 export default /*@ngInject*/class NewController{
   constructor($scope,$stateParams){
     this.image = _.find($scope.images.images,{_id: $stateParams.image});
-    this.template = `<figure>\n<img src="/api/images/${this.image.name}"/>\n<figcaption>Beispielunterschrift</figcaption>\n</figure>`;
+    this.template = `<figure>\n<img src="api/images/${this.image.name}"/>\n<figcaption>Beispielunterschrift</figcaption>\n</figure>`;
   }
 }
