303184bb849ae6b6c68f4f927a20ff40fd6ec60d
Tamara Cook <tamara.cook@icloud.com>
2015-11-04 02:32:50 +0100

    Use koa-unless for serving index.html
    Serving middleware executes only when path does not start with /dist/ or /api/.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 'use strict';
 const koa=require('koa');
 const send = require('koa-send');
+const unless = require('koa-unless');
 const mongoose=require('mongoose');
 const api=require('./api');
 const config=require('config');
 const server=config.get('server');
 const db=config.get('db');
 const assets=config.get('assets');
+function *serve(){
+  yield send(this,assets.index,{root: assets.root});
+}
+serve.unless = unless;
 const app=koa();
 require('koa-qs')(app);
 require('koa-onerror')(app);
@@ -15,11 +20,7 @@ if(config.get('logging')){
 }
 app.use(api.routes())
 .use(api.allowedMethods())
-.use(function *(){
-  if(!this.url.startsWith('/dist/')){
-    yield send(this,assets.index,{root: assets.root});
-  }
-})
+.use(serve.unless({path: ['/dist/','/api/']}))
 .listen(server.port,server.host,function(){
   console.log(`listening on ${server.host}:${server.port}`);
 });
