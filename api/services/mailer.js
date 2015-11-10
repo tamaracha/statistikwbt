11983a565ab4ae8bb8eaccdca60675dbee293045
Tamara Cook <tamara.cook@icloud.com>
2015-11-10 01:26:28 +0100

    Update deps
    * angular-permission
    * babel-loader
    * mongoose
    * nodemailer

--- api/services/mailer.js
+++ api/services/mailer.js
@@ -1,7 +1,5 @@
 'use strict';
-const bluebird=require('bluebird');
 const mailer=require('nodemailer');
 const mailConfig=require('config').get('email');
 const transport=mailConfig ? mailer.createTransport(mailConfig) : mailer.createTransport();
-bluebird.promisifyAll(transport);
 module.exports=transport;
