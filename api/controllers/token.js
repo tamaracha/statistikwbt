8c4d2648d8b6111be3c01e22ef2407a9a0085763
Tamara Cook <tamara.cook@icloud.com>
2015-11-12 07:02:13 +0100

    Send role in body with token

--- api/controllers/token.js
+++ api/controllers/token.js
@@ -5,6 +5,7 @@ module.exports.new=function *(){
   const token = tokenize.sign(this.state.user);
   this.body={
     token: token,
-    _id: this.state.user._id
+    _id: this.state.user._id,
+    role: this.state.user.role
   };
 };
