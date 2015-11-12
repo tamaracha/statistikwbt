7abc7c5021f4acacd9d72c434bcd645851ef996d
Tamara Cook <tamara.cook@icloud.com>
2015-11-12 06:31:35 +0100

    Include markdown

---
+++ src/markdown/markdown-provider.js
@@ -0,0 +1,46 @@
+import angular from 'angular';
+import mdfigcaption from 'mdfigcaption';
+import deflist from 'markdown-it-deflist';
+export default /*@ngInject*/class MarkdownProvider{
+  constructor(markdownit){
+    this.config = {
+      preset: 'default',
+      options: {
+        html: true,
+        typographer: true,
+        quotes: '„“‚‘',
+        breaks: true
+      },
+      plugins: [mdfigcaption, deflist]
+    };
+    this.markdownit = markdownit;
+  }
+  get preset(){
+    return this.config.preset;
+  }
+  set preset(val){
+    this.config.preset = val;
+    return this;
+  }
+  get options(){
+    return this.config.options;
+  }
+  set options(val){
+    angular.extend(this.config.options, val);
+    return this;
+  }
+  get plugins(){
+    return this.config.plugins;
+  }
+  use(val){
+    this.config.plugins.push(val);
+    return this;
+  }
+  $get(){
+    const md = this.markdownit(this.config.preset,this.config.options);
+    angular.forEach(this.config.plugins,(plugin) => {
+      md.use(plugin);
+    });
+    return md;
+  }
+}
