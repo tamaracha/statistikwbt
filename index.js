1972a65c4bd9de8a507f8dbe84856b1cc902e8c0
Tamara Cook <tamara.cook@icloud.com>
2015-11-17 02:50:27 +0100

    Serve docs and swagger stuff
    * frontend docs created by jsdoc
    * swagger-ui and spec
    * can be configured through config

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const assets=config.get('assets');
 const koa=require('koa');
 const unless = require('koa-unless');
 const Jade = require('koa-jade');
+const mount = require('koa-mount');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const swaggerSpec = yaml.load(fs.readFileSync('./api/swagger.yml','utf8'));
 const jade = new Jade({
   viewPath: 'src/',
   locals: {
@@ -19,6 +23,9 @@ function *render(){
   this.render('index');
 }
 render.unless = unless;
+function *swagger(){
+  this.body = swaggerSpec;
+}
 const api=require('./api');
 const app=koa();
 require('koa-qs')(app);
@@ -27,12 +34,16 @@ if(config.get('logging')){
   app.use(require('koa-morgan').middleware('dev'));
 }
 if(assets.serve){
-  app.use(require('koa-mount')(assets.root,require('koa-static')(__dirname+'/dist')));
+  const koaStatic = require('koa-static');
+  app.use(mount(assets.root,koaStatic(__dirname+'/dist')))
+  .use(mount('/docs',koaStatic(__dirname+'/docs')))
+  .use(mount('/swagger',koaStatic(__dirname+'/swagger-ui/dist')));
 }
-app.use(api.routes())
+app.use(mount('/api-docs.json',swagger))
+.use(api.routes())
 .use(api.allowedMethods())
 .use(jade.middleware)
-.use(render.unless({path: [/api/,new RegExp(assets.root)]}))
+.use(render.unless({path: [/api/,new RegExp(assets.root), /docs/, /swagger/]}))
 .listen(server.port,server.host,function(){
   console.log(`listening on ${server.host}:${server.port}`);
 });
