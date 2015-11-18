f0d92fd74eeb935c6abfe991cfb6d37a4f8847bb
Tamara Cook <tamara.cook@icloud.com>
2015-11-18 13:25:30 +0100

    Move meta setup out of meta controller :

--- api/controllers/meta.js
+++ api/controllers/meta.js
@@ -1,15 +1,7 @@
 'use strict';
-const _ = require('lodash');
-const bluebird = require('bluebird');
-const fs = require('fs');
-bluebird.promisifyAll(fs);
-const path = require('path');
-const yaml = require('js-yaml');
-const assets = require('config').get('assets');
 const models = require('../models');
 const jsonpatch=require('fast-json-patch');
 const $ = module.exports = {};
-setup();
 
 $.index = function *index(){
   const meta = yield models.Meta.find({},'-body').sort('position').lean().exec();
@@ -38,7 +30,7 @@ $.update = function *update(){
     this.status=200;
   }
   else{
-    this.throw('patch not successful');
+    this.throw('patch not successful',400);
   }
 };
 
@@ -47,28 +39,3 @@ $.destroy = function *destroy(){
   this.assert(meta,404);
   this.status = 200;
 };
-
-function setup(){
-  return models.Meta.find().exec()
-  .then((meta) => {
-    if(meta.length > 0) throw Error('already set up');
-    return fs.readFileAsync(path.join(__dirname,'../meta.yml'),'utf8');
-  })
-  .then((file) => {
-    const docs = yaml.load(file);
-    const home = _.findIndex(docs,{_id: 'home'});
-    if(home >= -1){
-      docs[home].title = assets.title;
-      docs[home].menu = assets.title;
-    }
-    return models.Meta.create(docs);
-  })
-  .then(
-    (data) => {
-      console.log('all right');
-    },
-    (e) => {
-      console.log(e);
-    }
-  );
-}
