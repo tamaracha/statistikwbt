524222cf0e7722edbe0db31700fc942c08290694
Tamara <Tamara.Cook@icloud.com>
2015-12-17 15:39:41 +0100

    Add videos state to author mode

---
+++ src/main/author/videos/videos-controller.js
@@ -0,0 +1,48 @@
+export default /*@ngInject*/class ImagesController{
+  constructor($scope, FileUploader, Restangular, $localStorage){
+    this.videos = Restangular.all('videos');
+    this.searchTerms = {};
+    this.uploader = new FileUploader({
+      url: 'api/videos',
+      onBeforeUploadItem: (i) => {
+        i.headers['Authorization'] = 'bearer '+$localStorage.token;
+        return i;
+      }
+    });
+    this.fields = [
+      {
+        type: 'horizontalInput',
+        key: 'search',
+        templateOptions: {
+          label: 'Suche',
+          type: 'search'
+        },
+        modelOptions: {
+          updateOn: 'default blur',
+          debounce: {
+            default: 300,
+            blur: 0
+          }
+        }
+      },
+      {
+        type: 'horizontalCheckbox',
+        key: 'me',
+        templateOptions: {
+          label: 'Nur von mir hochgeladen'
+        }
+      }
+    ];
+    $scope.$watchCollection('images.searchTerms',(val,oldVal) => {
+      if(val === oldVal){return;}
+      this.search();
+    });
+  }
+  search(){
+    return this.videos.getList(this.searchTerms)
+    .then((data) => {
+      this.videos = data;
+      return data;
+    });
+  }
+}
