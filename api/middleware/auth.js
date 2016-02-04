717082c134632de1e860a66519dabc996e7d26dc
Tamara Cook <tamara.cook@icloud.com>
2016-02-04 12:14:41 +0100

    Fix username in auth middleware

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,7 +1,6 @@
 'use strict';
 const models = require('../models');
 const basicAuth = require('basic-auth');
-const username = require('config').get('username');
 
 module.exports = function *auth(next){
   const login = basicAuth(this.request);
@@ -9,9 +8,12 @@ module.exports = function *auth(next){
   this.assert(typeof login.name === 'string','name is not a string',500);
   this.assert(typeof login.pass === 'string','pass is not a string',500);
   const user = yield models.User.findOne(null, '+password')
-  .where(username, login.name)
+  .or([
+    {email: login.name},
+    {kennung: login.name}
+  ])
   .exec();
-  this.assert(user, 404, username+' not found');
+  this.assert(user, 404, this.header['x-username']+' not found');
   const isMatch = yield user.validatePassword(login.pass);
   this.assert(isMatch,'incorrect password',401);
   this.state.user = user;
