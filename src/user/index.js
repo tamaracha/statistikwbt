e66b7b7e93626e6764d9737c70bec0ac7e0cd708
Tamara Cook <tamara.cook@icloud.com>
2016-02-06 03:34:43 +0100

    Turn user to angular module

--- src/user/index.js
+++ src/user/index.js
@@ -1,8 +1,17 @@
+import angular from 'angular';
 import interceptor from './user-interceptor';
 import model from './user-model';
 import validator from './user-check-validator';
-export {
-  interceptor,
-  model,
-  validator
-};
+
+/**
+ * @module wbt.user
+ */
+
+export default angular.module('wbt.user', [])
+.config(['$httpProvider', function($httpProvider){
+  $httpProvider.interceptors.push('userInterceptor');
+}])
+.factory('userInterceptor',interceptor)
+.service('user',model)
+.directive('userCheck',validator)
+.name;
