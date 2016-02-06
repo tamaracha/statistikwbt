7c33af7478e045ef75a4c8ab021d95ffd2e6e743
Tamara Cook <tamara.cook@icloud.com>
2016-02-06 03:34:18 +0100

    Add jsdoc comment to youtube module

--- src/youtube/index.js
+++ src/youtube/index.js
@@ -2,6 +2,9 @@ import angular from 'angular';
 import youtubeDirective from './youtube-directive';
 import youtube from './youtube-service';
 
+/**
+ * @module wbt.youtube
+ */
 export default angular.module('wbt.youtube', [])
 .directive('youtube',youtubeDirective)
 .factory('youtube',youtube)
