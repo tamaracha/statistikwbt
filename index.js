3bbd9120bdfd82e751166113565dd6aba0f242a0
Tamara Cook <tamara.cook@icloud.com>
2016-02-10 12:02:47 +0100

    Prerender indexPage on app start

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const config=require('config');
 const server=config.get('server');
 const db=config.get('db');
 const assets=config.get('assets');
+const dots = require('dot').process({path: 'templates'});
+const indexHtml = dots.index({
+  title: assets.title,
+  base: assets.base,
+  username: config.get('username')
+});
 const koa=require('koa');
 const mongoose=require('mongoose');
 mongoose.Promise = require('bluebird');
 function *index(){
-  yield this.render('index', {
-    title: assets.title,
-    base: assets.base,
-    env: process.env.NODE_ENV,
-    username: config.get('username')
-  });
+  this.body = indexHtml;
 }
 index.unless = require('koa-unless');
 const api=require('./api');
@@ -24,10 +25,9 @@ if(assets.serve){
   app.use(require('koa-mount')(assets.root, require('koa-static')(__dirname+'/dist')))
 }
 app
-.use(require('koa-dot')({path: __dirname+'/templates'}))
+.use(index.unless({path: [/api/,new RegExp(assets.root), /docs/, /swagger/]}))
 .use(api.routes())
 .use(api.allowedMethods())
-.use(index.unless({path: [/api/,new RegExp(assets.root), /docs/, /swagger/]}))
 .listen(server.port,server.host,function(){
   debug(`listening on ${server.host}:${server.port}`);
 });
