deee685c2ba8dbc4ab42a4616c9d4bb39591bdf7
Tamara Cook <tamara.cook@icloud.com>
2016-02-15 13:38:54 +0100

    Refactor removeModal

--- src/main/author/units/remove-modal/index.js
+++ src/main/author/units/remove-modal/index.js
@@ -1,7 +1,10 @@
 import templateUrl from './remove-modal.jade';
-export default function(scope){
+export default function(kind, scope){
   return {
     templateUrl,
+    controller: ['$scope', function($scope){
+      $scope.kind = kind;
+    }],
     scope
   };
 }
