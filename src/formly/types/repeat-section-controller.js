ec644dff1c5b7256c1978234e1f6fa18a80c4a75
Tamara Cook <tamara.cook@icloud.com>
2016-02-17 20:57:29 +0100

    Update formly repeatSection type

--- src/formly/types/repeat-section-controller.js
+++ src/formly/types/repeat-section-controller.js
@@ -1,18 +1,51 @@
 import angular from 'angular';
-import objectid from 'objectid-browser';
-export default function repeatSectionController($scope){
-  function copyFields(fields) {
-    return angular.copy(fields);
-  }
-  function addNew() {
-    $scope.model[$scope.options.key] = $scope.model[$scope.options.key] || [];
-    const repeatsection = $scope.model[$scope.options.key];
-    //let lastSection = repeatsection[repeatsection.length - 1];
-    const newsection = {_id: objectid()};
-    repeatsection.push(newsection);
-  }
+var unique = 1;
+export default function RepeatSectionController($scope){
   $scope.formOptions = {formState: $scope.formState};
   $scope.addNew = addNew;
+  
   $scope.copyFields = copyFields;
+  
+  
+  function copyFields(fields) {
+    fields = angular.copy(fields);
+    addRandomIds(fields);
+    return fields;
+  }
+  
+  function addNew() {
+    if(!$scope.newText){return;}
+    $scope.model[$scope.options.key] = $scope.model[$scope.options.key] || [];
+    var repeatsection = $scope.model[$scope.options.key];
+    //var lastSection = repeatsection[repeatsection.length - 1];
+    var newsection = {text: $scope.newText};
+    /*
+    if (lastSection) {
+      newsection = angular.copy(lastSection);
+    }
+    */
+    repeatsection.push(newsection);
+    $scope.newText = '';
+  }
+  
+  function addRandomIds(fields) {
+    unique++;
+    angular.forEach(fields, function(field, index) {
+      if (field.fieldGroup) {
+        addRandomIds(field.fieldGroup);
+        return; // fieldGroups don't need an ID
+      }
+      
+      if (field.templateOptions && field.templateOptions.fields) {
+        addRandomIds(field.templateOptions.fields);
+      }
+      
+      field.id = field.id || (field.key + '_' + index + '_' + unique + getRandomInt(0, 9999));
+    });
+  }
+  
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 }
-repeatSectionController.$inject = ['$scope'];
+RepeatSectionController.$inject = ['$scope'];
