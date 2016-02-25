4eb69fc2c33610d59c09b995aefcb04d83fa7d7e
Tamara Cook <tamara.cook@icloud.com>
2016-02-25 14:24:34 +0100

    Update status codes in auth middleware

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -6,12 +6,12 @@ const username = require('config').get('username');
 module.exports = function *auth(next){
   const login = basicAuth(this.request);
   this.assert(login,'no authorization found',401);
-  this.assert(typeof login.name === 'string','name is not a string',500);
-  this.assert(typeof login.pass === 'string','pass is not a string',500);
+  this.assert(typeof login.name === 'string','name is not a string',400);
+  this.assert(typeof login.pass === 'string','pass is not a string',400);
   const user = yield models.User.findOne(null, '+password')
   .where(username, login.name)
   .exec();
-  this.assert(user, 404, username+' not found');
+  this.assert(user, 401, username+' not found');
   const isMatch = yield user.validatePassword(login.pass);
   this.assert(isMatch,'incorrect password',401);
   this.state.user = user;
