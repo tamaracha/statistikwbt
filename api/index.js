08c86702b098d20d128f2c5129b72e50a9e58be8
Tamara Cook <tamara.cook@icloud.com>
2016-02-25 14:26:25 +0100

    Add JWT to views and watches index route

--- api/index.js
+++ api/index.js
@@ -89,8 +89,8 @@ api.use('/guesses',jwt,roles.can('access content'),guesses.routes());
 
 api.post('/ratings',jwt,roles.can('access content'),body,ctrl.rating.create);
 api.post('/comments',jwt,roles.can('access content'),body,ctrl.comment.create);
-api.get('/views', ctrl.view.index);
-api.get('/watches', ctrl.watch.index);
+api.get('/views', jwt, roles.can('edit content'), ctrl.view.index);
+api.get('/watches', jwt, roles.can('edit content'), ctrl.watch.index);
 api.post('/watches',jwt,body,ctrl.watch.create);
 
 // images
