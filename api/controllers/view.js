73c543dfcb061d861cde134cb61e78cd98774fd0
Tamara Cook <tamara.cook@icloud.com>
2016-02-26 13:37:04 +0100

    Send cleaned views and watches in JSON or CSV

--- api/controllers/view.js
+++ api/controllers/view.js
@@ -1,14 +1,48 @@
 'use strict';
+const remove = require('lodash/remove');
+const bluebird = require('bluebird');
+const csv = require('fast-csv');
+const writeToString = bluebird.promisify(csv.writeToString);
 const models = require('../models');
 const $ = module.exports = {};
 
+function transform(row){
+  return {
+    user: row.user.kennung || row.user.email,
+    createdAt: row._id.getTimestamp().toISOString(),
+    unit: row.unit.title,
+    topic: row.topic ? row.unit.topics.id(row.topic).title : 'description'
+  };
+}
 $.index = function *index(){
   const views = yield models.View.find()
   .sort({
     user: 1,
     _id: 1
   })
-  .select('-__v')
+  .populate('user','kennung email')
+  .populate('unit','title topics._id topics.title')
   .exec();
-  this.body = views;
+  remove(views, function(v){
+    if(!v.unit || !v.user){return true;}
+    return false;
+  });
+  if(this.header.accept.indexOf('json') === -1){
+    this.attachment('Clicks');
+  }
+  switch(this.query.format){
+    default:
+      this.body = views.map(transform);
+      break;
+    case 'csv':
+      this.type = 'text/csv';
+      this.body = yield writeToString(views, {
+        transform,
+        quoteColumns: this.query.quoteColumns ? true : false,
+        delimiter: this.query.delimiter || ',',
+        headers: this.query.headers ? true : false,
+        includeEndRowDelimiter: this.query.includeEndRowDelimiter ? true : false 
+      });
+      break;
+  }
 };
--- api/controllers/watch.js
+++ api/controllers/watch.js
@@ -1,6 +1,28 @@
 'use strict';
+const remove = require('lodash/remove');
+const bluebird = require('bluebird');
+const csv = require('fast-csv');
+const writeToString = bluebird.promisify(csv.writeToString);
 const models = require('../models');
 const $ = module.exports = {};
+
+function transform(row){
+  const output = {
+    user: row.user.kennung || row.user.email,
+    createdAt: row._id.getTimestamp().toISOString(),
+    state: row.state,
+    time: row.time
+  };
+  if(row.list){
+    output.type = 'list';
+    output.id = row.list;
+  }
+  else{
+    output.type = 'video';
+    output.id = row.video;
+  }
+  return output;
+}
 
 $.index = function *index(){
   const watches = yield models.Watch.find()
@@ -8,9 +30,30 @@ $.index = function *index(){
     user: 1,
     _id: 1
   })
-  .select('-__v')
+  .populate('user','kennung email')
   .exec();
-  this.body = watches;
+  remove(watches, function(v){
+    return !v.user ? true : false;
+  });
+  if(this.header.accept.indexOf('json') === -1){
+    this.attachment('Videos');
+  }
+  switch(this.query.format){
+    default: {
+      this.body = watches.map(transform);
+      break;
+    }
+    case 'csv':
+      this.type = 'text/csv';
+      this.body = yield writeToString(watches, {
+        transform,
+        quoteColumns: this.query.quoteColumns ? true : false,
+        delimiter: this.query.delimiter || ',',
+        headers: this.query.headers ? true : false,
+        includeEndRowDelimiter: this.query.includeEndRowDelimiter ? true : false 
+      });
+      break;
+  }
 };
 
 $.create = function *create(){
